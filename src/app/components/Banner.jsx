606b08ee9825a5c9a3151e5aacc6b0a522af4a3b
vinod859 <vk6560920@gmail.com>
2023-05-13 14:29:16 +0530

    20th commit Vinod

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -8,15 +8,15 @@ const Banner = () => {
         <div>
             <Carousel showStatus={false} showThumbs={false} centerMode={false} >
                 <div>
-                    <img src={require('../assets/img/banner/b1.webp')} />
+                <img src= '	https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/7ebae6ab4be6cda7.jpg?q=50'/>
                     {/* <p className="legend">Legend 1</p> */}
                 </div>
                 <div>
-                    <img src={require('../assets/img/banner/b2.webp')} />
+                <img src=	'https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/290544dd3545fc19.jpg?q=50' />
                     {/* <p className="legend">Legend 2</p> */}
                 </div>
                 <div>
-                    <img src={require('../assets/img/banner/b3.webp')} />
+                <img src='https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/275a28281f9262c7.jpg?q=50' />
                     {/* <p className="legend">Legend 3</p> */}
                 </div>
             </Carousel>
